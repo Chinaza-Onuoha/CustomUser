c2c6cd072e6455cc7178a209cf8631c81ee2ca92
De-voice <vsignal751@gmail.com>
2022-04-14 06:13:21 +0100

    Added email and phoneNumber fields to the customUser class

--- src/com/azaproperties/CustomUser.java
+++ src/com/azaproperties/CustomUser.java
@@ -7,15 +7,18 @@ public class CustomUser {
     private String zipCode;
     private String ipAddress;
     private String password;
-    private String contact;
+    private String email;
+    private String phoneNumber;
 
-    public CustomUser(String ID, String name, String location, String zipCode, String ipAddress, String password,String contact) {
+    public CustomUser(String ID, String name, String location, String zipCode, String ipAddress, String password,String email, String phoneNumber) {
         this.ID = ID;
         this.name = name;
         this.location = location;
         this.zipCode = zipCode;
         this.ipAddress = ipAddress;
         this.password = password;
-        this.contact = contact;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+
     }
 }
